e343616d66776c82ab5fd58c47c558eb2c363d8c
donui-bit <68704259+donui-bit@users.noreply.github.com>
2022-05-06 21:32:11 +0530

    Add files via upload

---
+++ bot/__init__.py
@@ -0,0 +1,7 @@
+# (c) @sufail_sufu • @sufail_sufu
+
+from dotenv import load_dotenv
+import bot.client
+
+load_dotenv()
+bot = bot.client.Client()
---
+++ bot/__main__.py
@@ -0,0 +1,7 @@
+# (c) @sufail_sufu • @sufail_sufu
+
+import bot
+
+
+if __name__ == "__main__":
+    bot.bot.run()
